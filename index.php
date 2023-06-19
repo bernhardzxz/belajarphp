00f7d6c7d27bcba4ffa50383ee156a8da721492f
bernhardzxz <dermawan.bernhard099@gmail.com>
2023-06-20 00:31:32 +0700

    admin

--- index.php
+++ index.php
@@ -11,6 +11,7 @@
     <!-- My Icon Bootstarp -->
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.3/font/bootstrap-icons.css">
 
+    <!-- Online link -->
     <link href="/docs/5.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
 
     <!-- My CSS -->
